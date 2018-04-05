1cbd17817c5525d536f377a92cde299f1ada2446
Olaf Braun <info@braun-development.de>
2018-04-05 18:53:58 +0200

    Adding config files and some dirs

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author        Olaf Braun
+ * @copyright     2013-2017 Olaf Braun - Software Development
+ * @license       Braun-Development.de License <https://www.braun-development.de/lizenz.html>
+ */
+
+define("DOMAIN_PATH", "/");
+define("DOMAIN", "route-cms.de");
+define("HTML_FORMATTER", true);
+define("HTML_MINIFY", true);
---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author        Olaf Braun
+ * @copyright     2013-2017 Olaf Braun - Software Development
+ * @license       Braun-Development.de License <https://www.braun-development.de/lizenz.html>
+ */
+return [
+	"driver"   => "pdo_mysql",
+	"update"   => true,
+	'host'     => '',
+	'dbname'   => '',
+	'user'     => '',
+	'password' => ''
+];

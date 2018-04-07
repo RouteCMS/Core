e048811c735e7b2abd29f9fb7f5f253ae5abb8c0
Cyperghost <info@braun-development.de>
2018-04-07 18:23:50 +0200

    Remove cache dir

--- config/config.php
+++ config/config.php
@@ -5,8 +5,8 @@
  * @license       GNU Lesser General Public License <https://opensource.org/licenses/LGPL-3.0>
  */
 
-define("DOMAIN_PATH", "");
-define("DOMAIN", "route-cms.de");
+define("DOMAIN_PATH", "RouteCMS");
+define("DOMAIN", "localhost:63342");
 define("HTML_FORMATTER", true);
-define("HTML_MINIFY", true);
+define("HTML_MINIFY", false);
 define("DEV_MODE", true);
